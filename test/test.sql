324af390b019e5caab02d8a990c929bab6282063
zeeraw <zee@vall.in>
2018-11-01 14:55:08 +0100

    add binary primary key example

--- test/test.sql
+++ test/test.sql
@@ -2,39 +2,45 @@ DROP DATABASE IF EXISTS `modelgen_tests`;
 CREATE DATABASE `modelgen_tests`;
 USE `modelgen_tests`;
 
--- -- order is a builtin, tests should pass despite this
--- DROP TABLE IF EXISTS `order`;
--- -- only one field should not break despite the special cases.
--- CREATE TABLE `order` (
---   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- order is a builtin, tests should pass despite this
+-- only one field should not break despite the special cases.
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- DROP TABLE IF EXISTS `common_cases`;
--- CREATE TABLE `common_cases` (
---   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
---   `not_null_string` varchar(255) NOT NULL DEFAULT '',
---   `not_null_int` int(11) NOT NULL,
---   `null_string` int(11) DEFAULT NULL,
---   `null_int` int(11) DEFAULT NULL,
---   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---   `updated_at` TIMESTAMP NULL DEFAULT NULL,
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `common_cases`;
+CREATE TABLE `common_cases` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `not_null_string` varchar(255) NOT NULL DEFAULT '',
+  `not_null_int` int(11) NOT NULL,
+  `null_string` int(11) DEFAULT NULL,
+  `null_int` int(11) DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- DROP TABLE IF EXISTS `complex_cases`;
--- CREATE TABLE `complex_cases` (
---   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
---   `raw_json` json NOT NULL,
---   `size_enum` enum('X-SMALL','SMALL','MEDIUM','LARGE','X-LARGE') DEFAULT NULL,
---   `updated_at` TIMESTAMP NULL DEFAULT NULL,
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `complex_cases`;
+CREATE TABLE `complex_cases` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `raw_json` json NOT NULL,
+  `size_enum` enum('X-SMALL','SMALL','MEDIUM','LARGE','X-LARGE') DEFAULT NULL,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `string_primary`;
 CREATE TABLE `string_primary` (
-  -- `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `sid` VARCHAR(255) NOT NULL DEFAULT '',
+  `sid` VARCHAR(255) NOT NULL,
   `name` VARCHAR(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `binary_uuid_ordered`;
+CREATE TABLE `binary_uuid_ordered` (
+  `uuid` BINARY(16) NOT NULL,
+  `name` VARCHAR(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
